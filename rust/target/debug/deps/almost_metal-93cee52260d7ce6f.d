26bebb9120ce482380f7e3c26bd8d9d7687dc15e
𝘚.𝘊. 𝘓𝘰𝘳𝘦𝘯𝘵𝘻 <113468900+SCLorentz@users.noreply.github.com>
2025-07-09 19:36:23 -0300

    Delete rust/target directory

--- rust/target/debug/deps/almost_metal-93cee52260d7ce6f.d
+++
@@ -1,7 +0,0 @@
-/Users/felipes.c.lorentz/Documents/GitHub/rust-test/rust/target/debug/deps/libalmost_metal-93cee52260d7ce6f.rmeta: src/main.rs src/functions/mod.rs src/functions/base.rs
-
-/Users/felipes.c.lorentz/Documents/GitHub/rust-test/rust/target/debug/deps/almost_metal-93cee52260d7ce6f.d: src/main.rs src/functions/mod.rs src/functions/base.rs
-
-src/main.rs:
-src/functions/mod.rs:
-src/functions/base.rs:
